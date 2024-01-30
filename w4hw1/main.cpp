1c58b1f5c7ca83512aebcc90d2c01d71780cf8be
Mārtiņš P <mrcukurs73@gmail.com>
2024-01-30 15:13:53 +0200

    Week 4 Homework 1

---
+++ w4hw1/main.cpp
@@ -0,0 +1,38 @@
+// Martins_Prokuratovs_221RDB473
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+const int MAX_VALUE = 50;
+const int MIN_VALUE = 1;
+
+int main() {
+    int i;
+    vector<int> numbers, missingNumbers;
+    ifstream numberStream ("missing_2_numbers.txt");
+
+    // Read stream of file contents and save numbers of it
+    if ( numberStream.is_open() ) {
+        while ( numberStream >> i ) {
+            numbers.insert(numbers.begin(), i);
+        }
+    }
+
+    // Generate all possible numbers
+    for (int i = MIN_VALUE; i <= MAX_VALUE; i++) {
+        if (find(numbers.begin(), numbers.end(), i) == numbers.end()) {
+            // If number not in file, mark as missing
+            missingNumbers.insert(missingNumbers.begin(), i);
+        }
+    }
+
+
+    printf("Missing numbers: \n");
+    for (int i: missingNumbers) {
+        cout << i << '\n';
+    }
+}
